1d2ce60e7e73b302d4ae94358462e62a506a5c5f
Ricky Thomas <rickythomas.rt@gmail.com>
2015-01-21 15:12:43 -0500

    Article associations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,8 @@
 class Article < ActiveRecord::Base
-  # Remember to create a migration!
+  belongs_to :user
+  belongs_to :category
+
+  has_many :comments
+  has_many :article_tags
+  has_many :tags, through: :article_tags
 end
