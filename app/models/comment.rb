51cd775f9ef13aed5099e2d7c2d2e4bd5bcfcd06
Ricky Thomas <rickythomas.rt@gmail.com>
2015-01-21 15:17:19 -0500

    Comment model, with recursive nested comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,10 @@
 class Comment < ActiveRecord::Base
-  # Remember to create a migration!
+  belongs_to :article
+  belongs_to :user
+
+  validates :user, presence: true
+  validates :content, presence: true
+
+  has_many :replies, foreign_key: :parent_id, class_name: "Comment"
+  belongs_to :parent, foreign_key: :parent_id, class_name: "Comment"
 end
