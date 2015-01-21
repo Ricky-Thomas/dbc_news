797cff155dba310fa405e5627dec071d682dd1b4
Ricky Thomas <rickythomas.rt@gmail.com>
2015-01-21 15:43:23 -0500

    Adds migration table for users

--- db/migrate/20150121150819_create_users.rb
+++ db/migrate/20150121150819_create_users.rb
@@ -1,4 +1,20 @@
 class CreateUsers < ActiveRecord::Migration
   def change
+    create_table :users do |t|
+      t.string  :name
+      t.string  :email
+      t.string  :password_digest
+      t.integer :score
+      t.string  :phase
+      t.string  :city
+      t.string  :cohort
+      t.string  :pic_url
+      t.string  :facebook_url
+      t.string  :linkedin_url
+      t.string  :twitter_url
+      t.string  :github_url
+      t.string  :blog_url
+      t.timestamps
+    end
   end
 end
