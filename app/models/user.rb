deaf65d8f7c116fed2fa3fd52b30314630dc44c9
Ricky Thomas <rickythomas.rt@gmail.com>
2015-01-21 17:24:16 -0500

    Adds email validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ActiveRecord::Base
   has_many :articles
   has_many :commented_articles, through: :comments, source: :article
 
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true, format: {with: /[a-zA-Z]*[1-9]*@[a-z]*.[a-z]{3}/}
 end
