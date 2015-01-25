2606de0f193000c53090f8b9a4d4d3dbed93a351
Ricky-Thomas <rickythomas.rt@gmail.com>
2015-01-25 16:10:18 -0500

    Adds may_edit method to user class

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -6,13 +6,3 @@ get '/' do
   end
 end
 
-get '/articles' do
-  @articles = Article.all
-  erb :'article/all'
-end
-
-post '/articles' do
-  article = Article.create(params[:article])
-  article.parse_tags!(params[:tags])
-  redirect ("/article/#{article.id}")
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,8 @@ class User < ActiveRecord::Base
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true, format: {with: /[a-zA-Z]*[1-9]*@[a-z]*.[a-z]{3}/}
+
+  def may_edit?(item)
+    item.user_id == self.id
+  end
 end
